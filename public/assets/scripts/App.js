adf363dac9ba46bac34f3025b0343d235c72fe89
rawwwulea <mihairaulea@gmail.com>
2019-03-23 20:52:48 +0200

    Components working

--- public/assets/scripts/App.js
+++ public/assets/scripts/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import MenuDrawer from './components/menu-drawer';
-import TopBar from './components/topbar';
+//import TopBar from './components/topbar';
 import Content from './components/content';
-
+//<TopBar />
 class App extends Component {
   render() {
     return (
       <div>
         <MenuDrawer />
-        <TopBar />
+
         <Content />
       </div>
     );
